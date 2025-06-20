5f9f6e469ed21355ed002c6f2c81659916e0292d
mdmhrz <razufreelance@gmail.com>
2025-06-20 18:36:45 +0600

    finall push

--- src/components/Users.jsx
+++ src/components/Users.jsx
@@ -1,13 +1,53 @@
 import React, { useState } from 'react';
-import { useLoaderData } from 'react-router';
+import { FaEye } from 'react-icons/fa';
+import { MdDelete, MdEdit } from 'react-icons/md';
+import { Link, useLoaderData } from 'react-router';
+import Swal from 'sweetalert2';
 
 const Users = () => {
     const initialUsers = useLoaderData();
     const [users, setUsers] = useState(initialUsers)
     // console.log(users);
+
+
+    const handleDelete = (id) => {
+        Swal.fire({
+            title: "Are you sure?",
+            text: "You won't be able to revert this!",
+            icon: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#3085d6",
+            cancelButtonColor: "#d33",
+            confirmButtonText: "Yes, delete it!"
+        }).then((result) => {
+            if (result.isConfirmed) {
+
+                fetch(`http://localhost:3000/users/${id}`, {
+                    method: 'DELETE'
+                }).then(result => result.json()).then(data => {
+                    if (data.deletedCount) {
+
+                        const remainingUsers = users.filter(user => user._id !== id);
+                        setUsers(remainingUsers);
+
+                        Swal.fire({
+                            title: "Deleted!",
+                            text: "User has been deleted.",
+                            icon: "success"
+                        });
+                    }
+
+                    console.log('After delete', data);
+                })
+
+
+
+            }
+        });
+    }
     return (
         <div>
-            <h2>Total Users = {initialUsers.length}</h2>
+            <h2 className='text-center text-4xl mb-8'>Total Users = {users.length}</h2>
             <div className="overflow-x-auto">
                 <table className="table">
                     {/* head */}
@@ -17,9 +57,9 @@ const Users = () => {
                                 No.
                             </th>
                             <th>Name</th>
-                            <th>Job</th>
-                            <th>Favorite Color</th>
-                            <th></th>
+                            <th>Phone Number</th>
+                            <th>Email</th>
+                            <th>Action</th>
                         </tr>
                     </thead>
                     <tbody>
@@ -34,24 +74,33 @@ const Users = () => {
                                         <div className="avatar">
                                             <div className="mask mask-squircle h-12 w-12">
                                                 <img
-                                                    src="https://img.daisyui.com/images/profile/demo/2@94.webp"
+                                                    src={user.photo}
                                                     alt="Avatar Tailwind CSS Component" />
                                             </div>
                                         </div>
                                         <div>
-                                            <div className="font-bold">Hart Hagerty</div>
-                                            <div className="text-sm opacity-50">United States</div>
+                                            <div className="font-bold">{user.name}</div>
+                                            <div className="text-sm opacity-50">{user.address}</div>
                                         </div>
                                     </div>
                                 </td>
                                 <td>
-                                    Zemlak, Daniel and Leannon
-                                    <br />
-                                    <span className="badge badge-ghost badge-sm">Desktop Support Technician</span>
+                                    {user.number}
+
                                 </td>
-                                <td>Purple</td>
-                                <th>
-                                    <button className="btn btn-ghost btn-xs">details</button>
+                                <td>{user.email}</td>
+                                <th className='flex gap-2'>
+                                    <Link to={`/coffee/${user._id}`}>
+                                        <button className="btn join-item bg-yellow-600"><FaEye className='text-white ' /></button>
+                                    </Link>
+
+
+                                    <Link to={`/updateCoffee/${user._id}`}>
+                                        <button className="btn join-item bg-white text-black"><MdEdit className='' /></button>
+                                    </Link>
+
+
+                                    <button onClick={() => handleDelete(user._id)} className="btn join-item bg-red-700"><MdDelete className='' /></button>
                                 </th>
                             </tr>)
                         }
